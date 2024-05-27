7fc9722b90c79b37134cfabd7f2fcee509f2efe7
Prakash Pandey <prakash.pandey01@nagarro.com>
2024-05-27 15:31:19 +0530

    first commit

---
+++ src/main/java/com/database/springboot_database_k8s/SpringbootDatabaseK8sApplication.java
@@ -0,0 +1,22 @@
+package com.database.springboot_database_k8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootDatabaseK8sApplication {
+
+
+	@GetMapping
+	public String helloWorld() {
+		return "Hi THis is Prakash";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDatabaseK8sApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/database/springboot_database_k8s/SpringbootDatabaseK8sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.database.springboot_database_k8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDatabaseK8sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
